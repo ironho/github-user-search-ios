5657484df803a4199a8373c89517bc5ba06aef6e
cheolho <ch.lee@lawcompany.co.kr>
2023-05-09 16:55:59 +0900

    chore: 앱 실행 시 최초로 보여지는 viewController를 storyboard가 아닌 SceneDelegate에서 처리하도록 수정

---
+++ GithubUserSearch/GithubUserSearch/Presenter/UserScene/UserListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserListViewController.swift
+//  GithubUserSearch
+//
+//  Created by cheolho on 2023/05/09.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+    
+}
