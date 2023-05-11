6dd251d8cd1768cb4fe23b18095d55f13897c613
cheolho <ch.lee@lawcompany.co.kr>
2023-05-11 15:43:19 +0900

    chore: Github Login을 위해 생성한 App의 client id, secret, callback scheme 추가

---
+++ GithubUserSearch/GithubUserSearch/Common/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GithubUserSearch
+//
+//  Created by cheolho on 2023/05/11.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let githubClientID = "01ea350d3e38f7447adf"
+    static let githubSecret = "6783cd7e935db336b383040b53cb9d8f8a3d74d5"
+    static let githubCallbackUrlScheme = "com.ironho.githubusersearch"
+    
+}
